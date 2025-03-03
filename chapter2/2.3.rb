7cb240c2765d2ab4cb44808bf5b0efa2b6c3ece2
endo <endo@elplanning.co.jp>
2025-03-04 08:12:26 +0900

    #inspectのオーバーライド

--- chapter2/2.3.rb
+++ chapter2/2.3.rb
@@ -1,13 +1,31 @@
 class Number < Struct.new(:value)
+    def to_s
+        value.to_s
+    end
+    def inspect
+        "<<#{self}>>"
+    end
 end
 
 class Add < Struct.new(:left,:right)
+    def to_s
+        "#{left} + #{right}"
+    end
+    def inspect
+        "<<#{self}>>"
+    end
 end
 
 class Multiply < Struct.new(:left,:right)
+    def to_s
+        "#{left} * #{right}"
+    end
+    def inspect
+        "<<#{self}>>"
+    end
 end
 
 Add.new(
-    Multiply.new(Number.new(1),Multiply.new(2)),
-    Multiply.new(Number.new(3),Multiply.new(4))
+    Multiply.new(Number.new(1),Number.new(2)),
+    Multiply.new(Number.new(3),Number.new(4))
 )
