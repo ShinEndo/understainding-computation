13e61213d24daaf7f124fa56e29a71b6db546826
endo <endo@elplanning.co.jp>
2025-03-07 08:06:04 +0900

    add: 条件の作成

--- chapter2/2.3.rb
+++ chapter2/2.3.rb
@@ -185,4 +185,36 @@ Machine.new(
     { x: Number.new(2)}
 ).run
 
+class If < Struct.new(:condition, :consequence, :alternative)
+    def to_s
+        "if (#{condition}) { #{consequence} } else { #{alternative} }"
+    end
+    def inspect
+        "<<#{self}>>"
+    end
+    def reducible?
+        true
+    end
+    def reduce(environment)
+        if condition.reducible?
+            [If.new(condition.reduce(environment),consequence,alternative), environment]
+        else
+            case condition
+            when Boolean.new(true)
+                [consequence,environment]
+            when Boolean.new(false)
+                [alternative,environment]
+            end
+        end
+    end
+end
+
+Machine.new(
+    If.new(
+        Variable.new(:x),
+        Assign.new(:y, Number.new(1)),
+        Assign.new(:y, Number.new(2))
+    ),
+    { x: Boolean.new(true) }
+).run
 
