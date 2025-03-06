30028ee26a2279a5d84724dc5e29c22658859627
endo <endo@elplanning.co.jp>
2025-03-07 07:58:02 +0900

    fix: Machineの変更

--- chapter2/2.3.rb
+++ chapter2/2.3.rb
@@ -157,10 +157,32 @@ class Assign < Struct.new(:name, :expression)
     end
 end
 
-statement = Assign.new(:x, Add.new(Variable.new(:x),Number.new(1)))
-environment = { x: Number.new(2) }
-statement.reducible?
-statement, environment = statement.reduce(environment)
-statement, environment = statement.reduce(environment)
-statement, environment = statement.reduce(environment)
-statement.reducible?
+# statement = Assign.new(:x, Add.new(Variable.new(:x),Number.new(1)))
+# environment = { x: Number.new(2) }
+# statement.reducible?
+# statement, environment = statement.reduce(environment)
+# statement, environment = statement.reduce(environment)
+# statement, environment = statement.reduce(environment)
+# statement.reducible?
+
+Object.send(:remove_const, :Machine)
+
+class Machine < Struct.new(:statement, :environment)
+    def step
+        self.statement, self.environment = statement.reduce(environment)
+    end
+    def run
+        while statement.reducible?
+            puts "#{statement}, #{environment}"
+            step
+        end
+        puts "#{statement}, #{environment}"
+    end
+end
+
+Machine.new(
+    Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
+    { x: Number.new(2)}
+).run
+
+
